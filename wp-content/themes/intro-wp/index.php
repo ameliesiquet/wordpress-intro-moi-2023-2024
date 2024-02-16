6ede44f511412f0b6d09c4691dc8b2ef17fc07f8
Amélie <120989561+ameliesiquet@users.noreply.github.com>
2024-02-16 16:39:47 +0100

    Added dw_asset function

--- wp-content/themes/intro-wp/index.php
+++ wp-content/themes/intro-wp/index.php
@@ -4,8 +4,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="<?= get_template_directory_uri() . '/public/theme.css'?>">
-    <script src="<?= get_template_directory_uri() . '/public/script.js' ?>" ></script>
+    <link rel="stylesheet" href="<?= dw_asset('theme.css')?>">
+    <script src="<?= dw_asset('script.js') ?>"> </script>
     <title>Document</title>
 </head>
 <body>
