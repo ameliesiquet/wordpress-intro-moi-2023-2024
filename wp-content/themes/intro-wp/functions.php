c8553578bef8618bc3428f7fe990e6e6b823a838
Amélie <120989561+ameliesiquet@users.noreply.github.com>
2024-03-01 14:28:14 +0100

    Added homepage content injection

--- wp-content/themes/intro-wp/functions.php
+++ wp-content/themes/intro-wp/functions.php
@@ -1,7 +1,11 @@
 <?php
 
+// désctiver l'(editeur de texte gutenberg
+
+add_filter('use_block_editor_for_post', '__return_false');
 function dw_asset(string $file): string
-    {
-        return get_template_directory_uri() . '/public/'. $file;
-    }
+{
+    return get_template_directory_uri() . '/public/'. $file;
+}
+
 
