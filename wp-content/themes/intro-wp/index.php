3cb48ff05552aa128fc85dffcdc7cf220832da56
Amélie <120989561+ameliesiquet@users.noreply.github.com>
2024-03-01 14:58:22 +0100

    Added header&footer files

--- wp-content/themes/intro-wp/index.php
+++ wp-content/themes/intro-wp/index.php
@@ -1,24 +1,10 @@
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="<?= dw_asset('theme.css') ?>">
-    <!-- <script src="<?= dw_asset('script.js') ?>"></script> --->
-    <title>Document</title>
-</head>
-<body>
-<h1><?= get_bloginfo('name') ?></h1>
-<main class="page">
-    <div class="page_content">
-        <?php if (have_posts()) : while (have_posts()):the_post(); // Ouverture de the loop ?>
-        <h1>
-            <?= get_the_title(); ?>
-        </h1>
-            <?= get_the_content(); ?>
-        <?php endwhile; endif; // Fermeture de the loop?>
-    </div>
-</main>
-</body>
-</html>
+<?php get_header(); ?>
+    <main class="page">
+        <div class="page__content">
+            <?php if(have_posts()): while(have_posts()): the_post(); // Ouverture de "The Loop" de Wordpress ?>
+                <h1><?= get_the_title(); ?></h1>
+                <?= get_the_content(); ?>
+            <?php endwhile; endif; // Fermeture de "The Loop" de Wordpress ?>
+        </div>
+    </main>
+<?php get_footer(); ?>
